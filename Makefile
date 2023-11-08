892716fa10f15c8d7215c2b0a4c3c24d9a249b7d
sungmin-Roh <46482326+sungmin-Roh@users.noreply.github.com>
2023-11-08 06:47:34 +0000

    7

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: webhook server
+
+webhook:
+	@pm2 start webhook.py --name webhooks --interpreter python3
+
+server:
+	@pm2 start ../lab-socket-programming2/tcp-server.py --name tcp-receiver --interpreter python3 --watch
+
+clean:
+    pm2 stop all
+    pm2 delete all
---
+++ webhook.py
@@ -0,0 +1,22 @@
+from flask import Flask, json
+from flask_restful import Resource, Api
+
+import os
+
+buildBranch = 'master'
+buildPath = '/workspace/miniserv/lab-socket-programming2'
+
+buildCommand = 'cd ' + buildPath + ' && git stash && git pull origin ' + buildBranch
+
+app = Flask(__name__)
+api = Api(app)
+
+class setDeploy(Resource):
+    def post(self):
+        os.system(buildCommand)
+        return {'status': 'success'}
+
+api.add_resource(setDeploy, '/deploy')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
